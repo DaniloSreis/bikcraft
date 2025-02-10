48545ae215908070158824a740f110731f39c960
Danilo <danilosreis12@gmail.com>
2025-02-10 17:48:56 -0300

    parametros url

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = window.location.href;
   const href = link.href;
-  if(url.includes(href)) {
-    link.classList.add('ativo')
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
 }
 
 links.forEach(ativarLink);
+
+// Ativar items do orçamento
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(paramentro) {
+  const elemento = document.getElementById(paramentro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+paramentros.forEach(ativarProduto);
