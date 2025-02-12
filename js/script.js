d9cc5582dd754dea8da3d6c703482929e41efa46
Danilo <danilosreis12@gmail.com>
2025-02-12 17:43:40 -0300

    galeria imagens

--- js/script.js
+++ js/script.js
@@ -25,19 +25,37 @@ paramentros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button')
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls')
-  const resposta = document.getElementById(controls)
-  resposta.classList.toggle('ativo')
-  const ativa = resposta.classList.contains('ativo')
-  pergunta.setAttribute('aria-expanded', ativa)
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativo");
+  const ativa = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPergunta(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta)
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPergunta)
+perguntas.forEach(eventosPergunta);
+
+// Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
