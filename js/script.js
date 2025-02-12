537b647d0387cc273363152aea7ed822f3921cd7
Danilo <danilosreis12@gmail.com>
2025-02-12 17:18:58 -0300

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -22,3 +22,22 @@ function ativarProduto(paramentro) {
 }
 
 paramentros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle('ativo')
+  const ativa = resposta.classList.contains('ativo')
+  pergunta.setAttribute('aria-expanded', ativa)
+}
+
+function eventosPergunta(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPergunta)
